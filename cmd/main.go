295cb943544c89a4fa0d033d8a8c9f70c4cc3408
Brett Kohler <brettkohler93@gmail.com>
2023-11-26 19:49:07 -0600

    update to not require env in production

--- cmd/main.go
+++ cmd/main.go
@@ -2,16 +2,19 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/bkohler93/jamhubapi/internal/app"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Print("Working without .env file")
-		log.Println(err)
+	if os.Getenv("IN_PROD") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Print("Working without .env file")
+			log.Println(err)
+		}
 	}
 	app.RunApp()
 }
